bbe0b9a42b66854d98f9f29753515a2bb357abc3
Cory Kim <corykim0829@gmail.com>
2020-07-13 22:16:34 +0900

    [iOS] feat: ViewModelBinding protoocl

---
+++ iOS/EatUpAll/EatUpAll/ViewModel/ViewModelBinding.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModelBinding.swift
+//  EatUpAll
+//
+//  Created by Cory Kim on 2020/07/13.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewModelBinding {
+    associatedtype Key
+    typealias handler = (Key) -> Void
+    func updateNotify(handler: @escaping handler)
+}
