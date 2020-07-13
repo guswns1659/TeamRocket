703bd6f95e978a87c6d7a47b76746cd36d13ff41
HYUNJUN <guswns1659@gmail.com>
2020-07-13 21:48:03 +0900

    [BE] feat : 스프링 프로젝트 초기 셋팅
    - application.yml 파일 추가.
    - HelloController 생성.
    - SwaggerConfig 추가.

---
+++ BE/src/main/java/com/codesquad/rocket/RocketApplication.java
@@ -0,0 +1,13 @@
+package com.codesquad.rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketApplication.class, args);
+    }
+
+}
---
+++ BE/src/main/java/com/codesquad/rocket/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.codesquad.rocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .host("localhost:8080")
+            .select()
+            .build();
+    }
+}
