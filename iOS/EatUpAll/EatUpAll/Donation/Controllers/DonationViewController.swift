c42015d4c67a28affc148510cd83763751ce1638
Cory Kim <corykim0829@gmail.com>
2020-07-13 23:00:03 +0900

    [iOS] feat: ClosingDonationProjectDataSource 구현
    뷰모델 바인딩까지 적용하여 구현
    
    #48

--- iOS/EatUpAll/EatUpAll/Donation/Controllers/DonationViewController.swift
+++ iOS/EatUpAll/EatUpAll/Donation/Controllers/DonationViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 final class DonationViewController: UIViewController {
+    
+    @IBOutlet weak var closingDonationProjectCollectionView: ClosingDonationProjectCollectionView!
+    private var closingDonationProjectCollectionViewDataSource: ClosingDonationProjectCollectionViewDataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configure()
+        fetchDonationProjects()
+    }
+    
+    private func fetchDonationProjects() {
+        closingDonationProjectCollectionViewDataSource.updateDonationProjects([
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000),
+            DonationProject(title: "1111", subtitle: "22222", deadline: Date(), numberOfDonations: 3, currentAmount: 100000, goalAmount: 100000000)])
     }
 }
 
@@ -22,10 +39,18 @@ final class DonationViewController: UIViewController {
 extension DonationViewController {
     private func configure() {
         configureNavigation()
+        configureClosingDonationProjectCollectionViewDataSources()
     }
     
     private func configureNavigation() {
         navigationItem.title = "기부하기"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureClosingDonationProjectCollectionViewDataSources() {
+        closingDonationProjectCollectionViewDataSource = ClosingDonationProjectCollectionViewDataSource(handler: { (_) in
+            self.closingDonationProjectCollectionView.reloadData()
+        })
+        closingDonationProjectCollectionView.dataSource = closingDonationProjectCollectionViewDataSource
     }
 }
--- iOS/EatUpAll/EatUpAll/Donation/Views/ClosingDonationProject/ClosingDonationProjectCell.swift
+++ iOS/EatUpAll/EatUpAll/Donation/Views/ClosingDonationProject/ClosingDonationProjectCell.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 final class ClosingDonationProjectCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+            
+        configure()
+    }
+}
 
+// MARK:- Configuration
+
+extension ClosingDonationProjectCell {
+    private func configure() {
+        configureUI()
+    }
+    
+    private func configureUI() {
+        roundCorner(cornerRadius: 8.0)
+        drawShadow(color: .darkGray, offset: .init(width: 0.1, height: 0.1), radius: 3.0, opacity: 0.7)
+        clipsToBounds = true
+    }
 }
