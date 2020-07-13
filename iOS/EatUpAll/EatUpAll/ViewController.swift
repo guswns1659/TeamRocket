41a604cc9ed7732b6ecf5d58f83d8fff70c5a6ed
Cory Kim <corykim0829@gmail.com>
2020-07-13 20:09:39 +0900

    [iOS] chore: project 생성
    #13

---
+++ iOS/EatUpAll/EatUpAll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EatUpAll
+//
+//  Created by Cory Kim on 2020/07/13.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
