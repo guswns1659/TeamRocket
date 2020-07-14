264d83c62db49551a708e1ed0feea7282a1dfcd2
Cory Kim <corykim0829@gmail.com>
2020-07-14 22:31:03 +0900

    [iOS] feat: 기부 UseCase, Request, Endpoint 구현
    #61

---
+++ iOS/EatUpAll/EatUpAll/Networks/Request/Donation/DonationClosingProjectRequest.swift
@@ -0,0 +1,13 @@
+//
+//  DonationClosingProjectRequest.swift
+//  EatUpAll
+//
+//  Created by Cory Kim on 2020/07/14.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import Foundation
+
+struct DonationClosingProjectRequest: Request {
+    var path: String = EndPoint.donationClosingProjectURL
+}
