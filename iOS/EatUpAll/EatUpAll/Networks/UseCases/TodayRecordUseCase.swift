791175d7e2b26dca83c248668a6e95f6427ba350
delma <sdy2856@gmail.com>
2020-07-14 20:02:31 +0900

    [iOS] feat : 오늘 잘먹었습니다 관련한 Request, UseCase 객체 생성
    #25

---
+++ iOS/EatUpAll/EatUpAll/Networks/UseCases/TodayRecordUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  TodayRecordUseCase.swift
+//  EatUpAll
+//
+//  Created by delma on 2020/07/14.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct TodayRecordUseCase: UseCase {
+    var networkDispatcher: NetworkDispatcher = AF
+}
