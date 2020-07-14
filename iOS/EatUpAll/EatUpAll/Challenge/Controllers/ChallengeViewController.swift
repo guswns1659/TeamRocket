e4bf868cdaece7a3980e93964f347fb28b811427
Cory Kim <corykim0829@gmail.com>
2020-07-14 21:31:31 +0900

    [iOS] feat: cell size
    layout을 사용하여 구현
    
    #31

--- iOS/EatUpAll/EatUpAll/Challenge/Controllers/ChallengeViewController.swift
+++ iOS/EatUpAll/EatUpAll/Challenge/Controllers/ChallengeViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 final class ChallengeViewController: UIViewController {
+    
+    private enum Metric {
+        static let numberOfCellAtRow: CGFloat = 3
+        static let minimumSpacing: CGFloat = 1.0
+    }
 
     @IBOutlet weak var collectionView: ChallengeCollectionView!
     private var dataSource: ChallengeCollectionViewDataSource!
@@ -26,6 +31,18 @@ extension ChallengeViewController {
     private func configure() {
         configureUI()
         configureCollectionViewDataSource()
+        configureCollectionViewLayout()
+    }
+    
+    private func configureCollectionViewLayout() {
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumLineSpacing = Metric.minimumSpacing
+        layout.minimumInteritemSpacing = Metric.minimumSpacing
+        let width = view.frame.width
+        let cellWidth = (width - (Metric.numberOfCellAtRow + 1) * Metric.minimumSpacing) / Metric.numberOfCellAtRow
+        layout.itemSize = CGSize(
+            width: cellWidth,
+            height: cellWidth)
     }
     
     private func configureUI() {
