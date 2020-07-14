d3bfd8a345b1fa698dd1e21491ccdd4fe1567583
Cory Kim <corykim0829@gmail.com>
2020-07-14 14:21:06 +0900

    [iOS] refactor: 주석제거
    #51

--- iOS/EatUpAll/EatUpAll/Donation/Delegates/ClosingDonationProjectCollectionViewDelegate.swift
+++ iOS/EatUpAll/EatUpAll/Donation/Delegates/ClosingDonationProjectCollectionViewDelegate.swift
@@ -45,14 +45,9 @@ extension ClosingDonationProjectCollectionViewDelegate : UIScrollViewDelegate {
         targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         
         let cellWidthIncludingSpacing = frame.width * 0.9 + 16
-        // targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
-        // 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
         var offset = targetContentOffset.pointee
         let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
 
-        // scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
-        // index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
-        // 스크로로 방향을 체크하여 올림,내림을 사용하면 좀 더 자연스러운 페이징 효과를 낼 수 있다.
         var roundedIndex = round(index)
         if scrollView.contentOffset.x > targetContentOffset.pointee.x {
             roundedIndex = floor(index) }
