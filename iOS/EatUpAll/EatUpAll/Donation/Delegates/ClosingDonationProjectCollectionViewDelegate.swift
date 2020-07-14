b48b56a461eea5376ba8973fe0e74a3ea5bc9e5b
Cory Kim <corykim0829@gmail.com>
2020-07-14 13:25:21 +0900

    [iOS] feat: ClosingDonationProjectDelegate 구현
    #48

---
+++ iOS/EatUpAll/EatUpAll/Donation/Delegates/ClosingDonationProjectCollectionViewDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  ClosingDonationProjectCollectionViewDelegate.swift
+//  EatUpAll
+//
+//  Created by Cory Kim on 2020/07/13.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import UIKit
+
+final class ClosingDonationProjectCollectionViewDelegate: NSObject, UICollectionViewDelegateFlowLayout {
+    
+    let frame: CGSize
+    
+    init(frame: CGSize) {
+        self.frame = frame
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: frame.width * 0.9, height: 220)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        16
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        16
+    }
+}
