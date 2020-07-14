282048d07f744930ffe5b20634af72fc34093234
delma <sdy2856@gmail.com>
2020-07-14 12:26:48 +0900

    [iOS] feat : 이번주 빈그릇 컬렉션뷰 셀을 뷰컨트롤러에 등록
    #15

--- iOS/EatUpAll/EatUpAll/Home/HomeViewController.swift
+++ iOS/EatUpAll/EatUpAll/Home/HomeViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    @IBOutlet weak var emptyPlateCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        emptyPlateCollectionView.register(UINib(nibName: String(describing: EmptyPlateCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: EmptyPlateCollectionViewCell.self))
+        emptyPlateCollectionView.dataSource = self
+    }
+}
 
+// MARK: - UICollectionViewDataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: EmptyPlateCollectionViewCell.self), for: indexPath) as! EmptyPlateCollectionViewCell
+        cell.configureImage(UIImage(named: "logo")!)
+        return cell
     }
 }
