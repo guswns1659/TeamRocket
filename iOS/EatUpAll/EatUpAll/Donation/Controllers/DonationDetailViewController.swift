f11d4ca395e554568b28b6dd8775224af02a3c96
delma <sdy2856@gmail.com>
2020-07-15 17:13:49 +0900

    [iOS] feat : descriptionStackView 추가 및 이미지뷰 생성해 넣어줌
    #77

--- iOS/EatUpAll/EatUpAll/Donation/Controllers/DonationDetailViewController.swift
+++ iOS/EatUpAll/EatUpAll/Donation/Controllers/DonationDetailViewController.swift
@@ -19,7 +19,9 @@ class DonationDetailViewController: UIViewController {
     @IBOutlet weak var leftTime: UILabel!
     @IBOutlet weak var donatorCount: UILabel!
     @IBOutlet weak var donationRatio: UILabel!
+    @IBOutlet weak var descriptionStackView: UIStackView!
     
+    private var descriptionImages: [UIImage] = []
     private var donationProjectDetailUseCase: DonationProjectDetailUseCase!
     
     override func viewDidLoad() {
@@ -50,13 +52,36 @@ class DonationDetailViewController: UIViewController {
     private func configureData(data: DonationDetail) {
         let url = URL(string: data.mainURL)
         mainImage.kf.setImage(with: url)
-        self.hostCompany.text = data.titleWithCompany
-        self.projectTitle.text = data.title
-        self.currentMoney.text = "\(data.currentMoney)원"
-        self.targetMoney.text = "\(data.targetMoney)원"
-        self.donatorCount.text = "\(data.donators)명"
-        self.leftTime.text = "\(data.leftDay)일 \(data.leftHour)시간"
+        hostCompany.text = data.titleWithCompany
+        projectTitle.text = data.title
+        currentMoney.text = "\(data.currentMoney)원"
+        targetMoney.text = "\(data.targetMoney)원"
+        donatorCount.text = "\(data.donators)명"
+        leftTime.text = "\(data.leftDay)일 \(data.leftHour)시간"
         let percentage = Double(data.currentMoney) / Double(data.targetMoney)
         self.donationRatio.text = "\(Int(percentage * 100))%"
+        configureDescriptionStackView(images: data.descriptionURL)
+    }
+    
+    private func configureDescriptionStackView(images: [String]) {
+        descriptionImages = []
+        images.forEach { generateDescriptionImages(imageURL: $0) }
+        descriptionStackView.subviews.forEach { $0.removeFromSuperview() }
+        descriptionImages.forEach {
+            let imageView = UIImageView(image: $0)
+            descriptionStackView.addArrangedSubview(imageView)
+        }
+    }
+    
+    private func generateDescriptionImages(imageURL: String) {
+        let url = URL(string:imageURL)
+        KingfisherManager.shared.retrieveImage(with: url!, options: nil, progressBlock: nil, downloadTaskUpdated: nil) { (result) in
+            switch result {
+            case .success(let value):
+                self.descriptionImages.append(value.image)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
