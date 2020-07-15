d9d9f025e727971d17b10cbb7792a074681c76be
delma <sdy2856@gmail.com>
2020-07-15 16:16:25 +0900

    [iOS] feat : DonationDetailView에 받아온 데이터 표시
    #77

--- iOS/EatUpAll/EatUpAll/Donation/Controllers/DonationDetailViewController.swift
+++ iOS/EatUpAll/EatUpAll/Donation/Controllers/DonationDetailViewController.swift
@@ -17,12 +17,35 @@ class DonationDetailViewController: UIViewController {
     @IBOutlet weak var leftTime: UILabel!
     @IBOutlet weak var donatorCount: UILabel!
     
+    private var donationProjectDetailUseCase: DonationProjectDetailUseCase!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configure()
+    }
+    
+    private func configure() {
+        donationProjectDetailUseCase = DonationProjectDetailUseCase()
     }
 
     @IBAction func backButtonDidTap(_ sender: UIButton) {
         dismiss(animated: true)
+    }
+    
+    func fetchDonationDetailData(id: Int) {
+        let request = DonationProjectDetailRequest(id: id).asURLRequest()
+        donationProjectDetailUseCase.getResources(request: request, dataType: DonationDetail.self) { result in
+            switch result {
+            case .success(let donationDetail):
+                self.hostCompany.text = donationDetail.titleWithCompany
+                self.projectTitle.text = donationDetail.title
+                self.currentMoney.text = "\(donationDetail.currentMoney)"
+                self.targetMoney.text = "\(donationDetail.targetMoney)"
+                self.donatorCount.text = "\(donationDetail.donators)"
+                self.leftTime.text = "\(donationDetail.leftDay)일 \(donationDetail.leftHour)시간"
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
