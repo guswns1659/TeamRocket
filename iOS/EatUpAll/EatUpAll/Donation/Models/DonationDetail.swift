dbd8e3fd6df63f66d59e665afa83fc14383b7a15
delma <sdy2856@gmail.com>
2020-07-15 14:56:02 +0900

    [iOS] feat : 프로젝트 상세 데이터 모델 생성
    #77

---
+++ iOS/EatUpAll/EatUpAll/Donation/Models/DonationDetail.swift
@@ -0,0 +1,21 @@
+//
+//  DonationDetail.swift
+//  EatUpAll
+//
+//  Created by delma on 2020/07/15.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import Foundation
+
+struct DonationDetail: Codable {
+    private(set) var currentMoney: Int
+    private(set) var descriptionURL: [String]
+    private(set) var donators: Int
+    private(set) var id: Int
+    private(set) var leftDay: Int
+    private(set) var leftHour: Int
+    private(set) var targetMoney: Int
+    private(set) var title: String
+    private(set) var titleWithCompany: String
+}
