82096bb9b63380e3c5b32e6bfbe001b8d1883846
delma <sdy2856@gmail.com>
2020-07-15 19:57:56 +0900

    [iOS] feat : 기부액 선택화면 ui 구성
    #82

---
+++ iOS/EatUpAll/EatUpAll/Donation/Views/DonationDetail/DonateView.swift
@@ -0,0 +1,38 @@
+//
+//  DonateView.swift
+//  EatUpAll
+//
+//  Created by delma on 2020/07/15.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import UIKit
+
+class DonateView: UIView {
+
+    @IBOutlet var frameView: UIView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var donateButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        Bundle.main.loadNibNamed(String(describing: Self.self), owner: self, options: nil)
+        addSubview(frameView)
+        frameView.frame = self.bounds
+        configureUI()
+    }
+    
+    private func configureUI() {
+        containerView.roundCorner(cornerRadius: 15)
+        donateButton.roundCorner(cornerRadius: 10)
+    }
+}
