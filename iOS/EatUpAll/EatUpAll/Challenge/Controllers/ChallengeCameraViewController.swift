baaa18cabf2a0be94edc661b9210aba37ed82262
Cory Kim <corykim0829@gmail.com>
2020-07-15 21:57:42 +0900

    [iOS] feat: 카메라 VC, preview VC UI XIB 구현
    #84

---
+++ iOS/EatUpAll/EatUpAll/Challenge/Controllers/ChallengeCameraViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeCameraViewController.swift
+//  EatUpAll
+//
+//  Created by Cory Kim on 2020/07/15.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import UIKit
+
+final class ChallengeCameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ iOS/EatUpAll/EatUpAll/Challenge/Controllers/ChallengePreviewViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChallengePreviewViewController.swift
+//  EatUpAll
+//
+//  Created by Cory Kim on 2020/07/15.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import UIKit
+
+final class ChallengePreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func cancelButtonDidTap(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saveButtonDidTap(_ sender: Any) {
+        
+    }
+}
