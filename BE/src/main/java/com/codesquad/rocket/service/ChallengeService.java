877f02c2f50c1cbf3b64d1cfd1ba94ca282876b0
HYUNJUN <guswns1659@gmail.com>
2020-07-16 19:51:50 +0900

    [BE] feat : ~/image/{restaurantId} 로컬에서 기능 구현.

--- BE/src/main/java/com/codesquad/rocket/service/ChallengeService.java
+++ BE/src/main/java/com/codesquad/rocket/service/ChallengeService.java
@@ -1,17 +1,26 @@
 package com.codesquad.rocket.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.codesquad.rocket.domain.account.Account;
 import com.codesquad.rocket.domain.account.AccountRepository;
+import com.codesquad.rocket.domain.account.PointHistory;
+import com.codesquad.rocket.domain.account.PointOption;
 import com.codesquad.rocket.domain.challenge.Challenge;
 import com.codesquad.rocket.domain.challenge.ChallengeRepository;
 import com.codesquad.rocket.domain.challenge.Like;
+import com.codesquad.rocket.domain.restaurant.Restaurant;
+import com.codesquad.rocket.domain.restaurant.RestaurantRepository;
+import com.codesquad.rocket.utils.S3Uploader;
 import com.codesquad.rocket.web.dto.response.challenge.ChallengeDetailResponseDto;
 import com.codesquad.rocket.web.dto.response.challenge.ChallengeResponseDto;
+import com.codesquad.rocket.web.dto.response.challenge.ChallengeStatusResponseDto;
 import com.codesquad.rocket.web.dto.response.challenge.LikeResponseDto;
 import com.codesquad.rocket.web.dto.response.challenge.WeeklyTopLikeResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +31,8 @@ public class ChallengeService {
 
     private final AccountRepository accountRepository;
     private final ChallengeRepository challengeRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final S3Uploader s3Uploader;
     private final static String TRUE = "True";
 
     public WeeklyTopLikeResponseDto weeklyTopLike() {
@@ -110,5 +121,41 @@ public class ChallengeService {
     private boolean hasAccountInLikes(Account account, Challenge challenge) {
         return challenge.getLikes().stream()
             .anyMatch(like -> like.getName().equals(account.getName()));
+    }
+
+    public ChallengeStatusResponseDto addChallenge(Long restaurantId, MultipartFile multipartFile, String description) throws
+        IOException {
+        try {
+            Restaurant restaurant = restaurantRepository.findById(restaurantId).orElse(new Restaurant());
+            Account account = accountRepository.findAccountByName("delma").orElse(new Account());
+            String uploadUrl = s3Uploader.upload(multipartFile, "static");
+
+            Challenge challenge = Challenge.builder()
+                .account(account)
+                .createdAt(new Date())
+                .description(description)
+                .point(restaurant.getPoint())
+                .restaurantName(restaurant.getName())
+                .updatedAt(new Date())
+                .url(uploadUrl)
+                .build();
+
+            challengeRepository.save(challenge);
+            PointHistory pointHistory = PointHistory.builder()
+                .pointOption(PointOption.SAVE)
+                .createdAt(new Date())
+                .ecoPoint(200)
+                .build();
+            account.addPointHistory(pointHistory);
+            accountRepository.save(account);
+
+            return ChallengeStatusResponseDto.builder()
+                .status("200")
+                .build();
+        } catch (Exception e) {
+            return ChallengeStatusResponseDto.builder()
+                .status("400")
+                .build();
+        }
     }
 }
--- BE/src/main/java/com/codesquad/rocket/web/controller/ChallengeController.java
+++ BE/src/main/java/com/codesquad/rocket/web/controller/ChallengeController.java
@@ -1,14 +1,21 @@
 package com.codesquad.rocket.web.controller;
 
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.codesquad.rocket.service.ChallengeService;
 import com.codesquad.rocket.web.dto.response.challenge.ChallengeDetailResponseDto;
 import com.codesquad.rocket.web.dto.response.challenge.ChallengeResponseDto;
+import com.codesquad.rocket.web.dto.response.challenge.ChallengeStatusResponseDto;
 import com.codesquad.rocket.web.dto.response.challenge.LikeResponseDto;
 import com.codesquad.rocket.web.dto.response.challenge.WeeklyTopLikeResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -39,6 +46,14 @@ public class ChallengeController {
     public LikeResponseDto getLikeResult(@PathVariable Long id,
         @RequestParam(value = "liked") String liked) {
         return challengeService.getLikeResult(id, liked);
+    }
 
+    @PostMapping(path = "{restaurantId}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ChallengeStatusResponseDto upload(
+        @PathVariable Long restaurantId,
+        @RequestPart(value = "picture") MultipartFile file,
+        @RequestParam(value = "description") String description) throws IOException {
+
+        return challengeService.addChallenge(restaurantId, file, description);
     }
 }
