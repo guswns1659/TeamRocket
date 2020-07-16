2a061e9b95b1554acc3e724326c2143906e12b80
Cory Kim <corykim0829@gmail.com>
2020-07-16 14:13:04 +0900

    [iOS] hotfix: optional unwrapping 대응
    #90

--- iOS/EatUpAll/EatUpAll/Challenge/Controllers/ChallengeCameraViewController.swift
+++ iOS/EatUpAll/EatUpAll/Challenge/Controllers/ChallengeCameraViewController.swift
@@ -34,6 +34,10 @@ final class ChallengeCameraViewController: UIViewController {
     }
     
     @IBAction func dismissButtonDidTap(_ sender: Any) {
+        dismissController()
+    }
+    
+    private func dismissController() {
         dismiss(animated: true, completion: nil)
     }
 }
@@ -75,7 +79,10 @@ extension ChallengeCameraViewController {
             deviceTypes: [.builtInWideAngleCamera],
             mediaType: .video,
             position: .back)
-        let device = deviceDiscoverySession.devices.first!
+        guard let device = deviceDiscoverySession.devices.first else {
+            dismissController()
+            return
+        }
         currentCamera = device
     }
     
