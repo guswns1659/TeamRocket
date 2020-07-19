8b4f3b3771ed8139407c97c48c44d6b60acd5759
delma <sdy2856@gmail.com>
2020-07-19 20:13:18 +0900

    [iOS] feat : MapViewController 추가 및 화면 전환
    #133

---
+++ iOS/EatUpAll/EatUpAll/Map/Controllers/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  EatUpAll
+//
+//  Created by delma on 2020/07/19.
+//  Copyright © 2020 team rocket. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
